2116b270b08115a2947f57da4bb605bad04b44af
JanSvaton <svaton.jan@ssakhk.cz>
2022-04-25 13:23:01 +0200

    Update main.cpp
    dodelat tar.gz

--- main.cpp
+++ main.cpp
@@ -1,8 +1,29 @@
     #include <QCoreApplication>
+#include <iostream>
+#include <fstream>
+#include <QDir>
+#include <QFileInfo>
+#include <QDirIterator>
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
 
-    return a.exec();
+    QList<QString> filestomove;
+    QDirIterator it(QString("."), QStringList() << "*", QDir::Files, QDirIterator::Subdirectories);
+    while (it.hasNext())
+        filestomove.append(it.next());
+    QString filename="files.txt";
+    QFile file( filename );
+    if ( file.open(QIODevice::ReadWrite) )
+    {
+
+        QTextStream stream( &file );
+        foreach (QString file, filestomove) {
+            stream << file << Qt::endl;
+        }
+
+
+    }
+    return a.exec();  
 }
